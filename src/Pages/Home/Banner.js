372ad79028e88053583ba3d39d8adccba48de943
HP 840 <yasin80545859@.com>
2022-05-22 12:48:41 +0600

    Banner making complate

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+            
+            <div class="hero min-h-screen personal">
+  <div class="hero-content text-center info">
+    <div class="max-w-md">
+      <h1 class="text-3xl font-bold">Hallo! Welcome To Manufacturer-Co</h1>
+      <p class="py-6">We Provid whole selling Manufacturer items assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
